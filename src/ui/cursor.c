7b6063dabdb80485f210c9a3c0223ac240ac85d0
Michael Moffitt <mikejmoffitt@gmail.com>
2023-09-17 23:58:10 +0900

    Selection rectangle drawing sped up by assembly fill sub

--- src/ui/cursor.c
+++ src/ui/cursor.c
@@ -52,6 +52,9 @@ void xt_cursor_set(uint16_t x, uint16_t y, int16_t w, int16_t h, int16_t tile_hl
 	s_state.dirty = true;
 }
 
+// curosr_fill.a68
+void cursor_fill_sub(uint16_t w, uint16_t h, uint16_t val);
+/*
 static void draw_rect(uint16_t x, uint16_t y, int16_t w, int16_t h, uint16_t val)
 {
 	if ((x / XT_RENDER_CELL_W_PIXELS) + w >= XT_RENDER_NT_CHARS) w = XT_RENDER_NT_CHARS - 1 - (x / XT_RENDER_CELL_W_PIXELS);
@@ -65,7 +68,7 @@ static void draw_rect(uint16_t x, uint16_t y, int16_t w, int16_t h, uint16_t val
 			*nt_line++ = val;
 		}
 	}
-}
+}*/
 
 void xt_cursor_update(void)
 {
@@ -73,15 +76,13 @@ void xt_cursor_update(void)
 	s_state.dirty = false;
 
 	// Erase the cursor from before.
+//	draw_rect(s_state_prev.w, XT_RENDER_NT_CHARS - s_state_prev.w, 0, s_state_prev.h, 0x0000);
+//	draw_rect(0, XT_RENDER_NT_CHARS, s_state_prev.h, XT_RENDER_NT_CHARS - s_state_prev.h, 0x0000);
+	const uint16_t erase_draw_w = (s_state_prev.w > XT_RENDER_NT_CHARS) ? XT_RENDER_NT_CHARS : s_state_prev.w;
+	const uint16_t erase_draw_h = (s_state_prev.h > XT_RENDER_NT_CHARS) ? XT_RENDER_NT_CHARS : s_state_prev.h;
+	if (erase_draw_w > 0 && erase_draw_h > 0) cursor_fill_sub(erase_draw_w, erase_draw_h, 0x0000);
+
 	volatile uint16_t *nt1 = (volatile uint16_t *)XB_PCG_BG1_NAME;
-	for (int16_t ty = 0; ty < s_state_prev.h; ty++)
-	{
-		volatile uint16_t *nt_line = &nt1[ty * XT_RENDER_NT_CHARS];
-		for (int16_t tx = 0; tx < s_state_prev.w; tx++)
-		{
-			*nt_line++ = 0;
-		}
-	}
 
 	// Draw the full line highlight.
 	if (s_state.line_hl && !s_state_prev.line_hl)
@@ -115,14 +116,19 @@ void xt_cursor_update(void)
 		}
 	}
 
+
 	// Draw the cursor block
+	const uint16_t cursor_draw_w = (s_state.w > XT_RENDER_NT_CHARS) ? XT_RENDER_NT_CHARS : s_state.w;
+	const uint16_t cursor_draw_h = (s_state.h > XT_RENDER_NT_CHARS) ? XT_RENDER_NT_CHARS : s_state.h;
 	if (s_state.tile_hl < 0)
 	{
-		draw_rect(0, s_state.w, 0, s_state.h, XB_PCG_ATTR(0, 0, XT_CURSOR_HL_PAL, 0x81));
+		const uint16_t cursor_value = 0x0081;
+		cursor_fill_sub(cursor_draw_w, cursor_draw_h, cursor_value);
 	}
 	else
 	{
-		draw_rect(0, s_state.w, 0, s_state.h, XB_PCG_ATTR(0, 0, XT_CURSOR_HL_PAL, 0x82));
+		const uint16_t cursor_value = 0x0082;
+		cursor_fill_sub(cursor_draw_w, cursor_draw_h, cursor_value);
 		nt1[s_state.tile_hl] = XB_PCG_ATTR(0, 0, XT_CURSOR_HL_PAL, 0x81);
 	}
 
